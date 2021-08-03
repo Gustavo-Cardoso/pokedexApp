a30fff7a18e120d7aeca3f06f3b8e833a3c64f5b
Gustavo Alves Cardoso <cardosogustavo790@gmail.com>
2021-08-02 22:23:05 -0300

    Criação do Array dos Pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  //Criação array pokemon
+
+  listaPokemon= [
+    {
+      numero: '001',
+      nome: 'Bulbasaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      tipos: [
+        'Grass', 'Poison'
+      ]
+    },
+    {
+      numero: '002',
+      nome: 'Ivysaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      tipos: [
+        'Grass', 'Poison'
+      ]
+    },
+    {
+      numero: '002',
+      nome: 'Venusaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      tipos: [
+        'Grass', 'Poison'
+      ]
+    },
+    {
+      numero: '004',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      tipos: [
+        'Fire'
+      ]
+    }
+  ];
+
   constructor() {}
 
 }
